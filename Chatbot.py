413d563f92466ebde4baf884898597db7a39be98
Jonathan Díaz <77712004+Jonialen@users.noreply.github.com>
2023-11-20 21:08:48 -0600

    Update 2

--- Chatbot.py
+++ Chatbot.py
@@ -2,5 +2,13 @@ from Usuario import Usuario
 
 
 class Chatbot(Usuario):
-    def __init__(self):
-        super().__init()
+    def __init__(self, username, name, password, type_user, chats):
+        super().__init(username, name, password, type_user)
+        self.input_text = chats[0]
+        self.output_text = chats[1]
+
+    def get_input_text(self):
+        return self.input_text
+
+    def get_output_text(self):
+        return self.output_text
--- TraductorU.py
+++ TraductorU.py
@@ -1,7 +1,14 @@
 from Usuario import Usuario
-import ConsultasI
 
 
 class Traductor(Usuario):
-    def __init__(self, username, name, password, type_user):
-        super().__init()
+    def __init__(self, username, name, password, type_user, traducciones):
+        super().__init(username, name, password, type_user)
+        self.idioma1 = traducciones[0]
+        self.idioma2 = traducciones[1]
+
+    def get_idioma_original(self):
+        return self.idioma1
+
+    def get_idioma_final(self):
+        return self.idioma2
