d99768ffdd1ebfaa201a037787314b816440e1f5
Jonathan Díaz <77712004+Jonialen@users.noreply.github.com>
2023-11-20 21:02:13 -0600

    Update usuarios

--- TraductorU.py
+++ TraductorU.py
@@ -3,26 +3,5 @@ import ConsultasI
 
 
 class Traductor(Usuario):
-    def __init__(self):
+    def __init__(self, username, name, password, type_user):
         super().__init()
-
-    def mostrar_menu(self):
-        while True:
-            print(" Menú de Traductor ")
-            print("1. Realizar tarea de traducción")
-            print("2. Cerrar sesión")
-            opcion = input("Seleccione una opción: ")
-
-            if opcion == "1":
-                self.realizar_tarea_traduccion()
-            elif opcion == "2":
-                self.cerrar_sesion()
-                break
-            else:
-                print("Opción no válida")
-
-    def realizar_tarea_traduccion(self):
-        print("Realizando tarea de traducción...")
-
-        result = ConsultasI.Consultas().obtener_idiomas_usuario(3)
-        print(result)
--- Usuario.py
+++ Usuario.py
@@ -2,16 +2,21 @@ import database
 
 
 class Usuario:
-    def __init__(self):
-        self.usuario_actual = None
+    def __init__(self, username, name, password, type_user):
         self.db = database.Database('localhost', 'root', '', 'proyecto_2')
+        self.username = username
+        self.name = name
+        self.password = password
+        self.type = type_user
 
-    def iniciar_sesion(self, nombre, contrasena):
-        flag = self.db.verificar_usuario(nombre, contrasena)
-        if flag:
-            self.usuario_actual = nombre
-            return True
-        return False
+    def get_username(self):
+        return self.username
 
-    def cerrar_sesion(self):
-        self.usuario_actual = None
+    def get_name(self):
+        return self.name
+
+    def get_password(self):
+        return self.password
+
+    def get_type_user(self):
+        return self.type_user
