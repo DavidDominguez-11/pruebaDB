601e443b26d1cb9c735f2d09eee6add741cf16e5
Jonathan Díaz <77712004+Jonialen@users.noreply.github.com>
2023-11-20 21:23:02 -0600

    a

--- Chat.py
+++ Chat.py
@@ -1,6 +1,7 @@
 import g4f
 from colorama import Fore
 
+
 class Chat:
     def __init__(self):
         g4f.debug.logging = True  # enable logging
@@ -16,12 +17,11 @@ class Chat:
             stream=True,
         )
 
-        #print("tipo" , type(response))
+        # print("tipo" , type(response))
         for message in response:
             print(Fore.BLUE + message, flush=True, end='')
             toString = toString + message
         print(Fore.WHITE+" ")
-        #print("Tipo de to String :",type(toString))
-        
+        # print("Tipo de to String :",type(toString))
 
         return toString
